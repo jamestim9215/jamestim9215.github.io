ef545d8b216e828b6abed9efcb9f5af361a1e6f7
ray <ray.lin@gigabyte.com>
2020-07-21 15:55:46 +0800

    no message

--- aorus_test/assets/js/common.js
+++ aorus_test/assets/js/common.js
@@ -3,6 +3,6 @@ var FooterTemplate = 'template/common/footer.vue';
 var CopyrightAndLangTemplate = 'template/common/copyright_lang.vue';
 var BannerTemplate = 'template/index/banner.vue';
 var IndexProductLineTemplate = 'template/index/index_product_line.vue';
-var IndexExploreTemplate = 'template/index/index_Explore.vue';
+var IndexExploreTemplate = 'template/index/index_explore.vue';
 var IndexESportTemplate = 'template/index/index_esport.vue';
 var IndexJoinMemberTemplate = 'template/index/index_join_member.vue';
