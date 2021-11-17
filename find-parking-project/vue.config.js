687557f58edcb5462d89436bdec76c6388cb7bc4
findparkinglot <jamestim9215@hotmail.com.tw>
2021-11-17 10:58:37 +0800

    test

--- find-parking-project/vue.config.js
+++ find-parking-project/vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
     
   
     // 在yarn build時 生成檔案的目錄 default:'dist'
-    outputDir: "../findparkinglot",
+    outputDir: "../../dist",
     // outputDir: "../wwwroot",
   
     // 放置生成的靜態資源 (js、css、img、fonts) 的目錄。default: ''
