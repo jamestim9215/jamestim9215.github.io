89a6cafe9912e81a586517faec4f7d1379708fe9
Ray01 <ray01.lin@gigabyte.com>
2022-12-05 22:38:32 +0800

    test

--- group_buying/group_buying/vite.config.js
+++ group_buying/group_buying/vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? '/group_buying/group_buying/dist/' : './',
   build: {
     outDir: './dist',
     assetsDir: "assets",
