f232e8146b9dda0f5b8f8c9eb4555c2352b1d537
Ray <ray.lin@gigabyte.com>
2023-01-05 19:16:54 +0800

    Up

--- Phaser3/Game/js/GamePlay.js
+++ Phaser3/Game/js/GamePlay.js
@@ -17,9 +17,11 @@ class GamePlay extends Phaser.Scene{
         this.map = this.make.tilemap({ key: 'map' });
         this.tileset = this.map.addTilesetImage('nature-paltformer-tileset-16x16', 'tiles');
         this.sky = this.map.createLayer('sky', this.tileset, 0, 0);
+        this.tree = this.map.createLayer('tree', this.tileset, 0, 0);
         this.ground = this.map.createLayer('ground', this.tileset, 0, 0).setScale(2);
 
         this.sky.setScale(2);
+        this.tree.setScale(2);
         this.ground.setScale(2);
         this.ground.setCollisionByExclusion([-1])
         
@@ -84,7 +86,7 @@ class GamePlay extends Phaser.Scene{
 
             if ((this.cursors.up.isDown || this.cursors.space.isDown  || this.keys.W.isDown) && this.player.body.velocity.y == 0)
             {
-                this.player.setVelocityY(-300);
+                this.player.setVelocityY(-380);
                 this.player.status = 'Jump';
                 this.player.anims.play('CharacterJump', true);
             }
