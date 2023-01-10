242c9b14e37234f7eb46b2c442e41ecf968e7da2
Ray <ray.lin@gigabyte.com>
2023-01-10 10:06:06 +0800

    UP

--- Phaser3/RPG/js/GameHome.js
+++ Phaser3/RPG/js/GameHome.js
@@ -16,13 +16,21 @@ class GameHome extends Phaser.Scene{
 
         this.background = this.add.tileSprite(config.width/2.2, config.height/2.2, config.width*2.2, config.height*2.2, 'background').setScale(0.5);
 
-        this.PlayButton = this.add.sprite(config.width / 2, config.height / 2 + 50, 'GameButton').setInteractive().setOrigin(0.5);
-        this.anims.create({
-            key: "ButtonShow",
-            frames: this.anims.generateFrameNumbers('GameButton', { frames: [24] }),
-            frameRate: 5,
-            repeat: -1     // 循環播放 -1 
-        })
+        this.skinIndex = 1;
+
+        this.PlayerSkin = this.add.sprite(config.width / 2, config.height / 3, 'Character'+this.skinIndex).setInteractive().setOrigin(0.5).setScale(2);
+
+        this.ChangeSkinButton = this.add.sprite(config.width / 2, config.height / 3 + 50, 'GameButton').setInteractive().setOrigin(0.5);
+        // this.anims.create({
+        //     key: "ButtonShow",
+        //     frames: this.anims.generateFrameNumbers('GameButton', { frames: [24] }),
+        //     frameRate: 5,
+        //     repeat: -1     // 循環播放 -1 
+        // })
+        // this.PlayButton.anims.play('ButtonShow');
+        
+
+        this.PlayButton = this.add.sprite(config.width / 2, config.height / 3 * 2 + 50, 'GameButton').setInteractive().setOrigin(0.5);
         this.anims.create({
             key: "ButtonDown",
             frames: this.anims.generateFrameNumbers('GameButton', { frames: [24,24,25,26,25,24,24] }),
@@ -31,7 +39,10 @@ class GameHome extends Phaser.Scene{
         })
         this.PlayButton.anims.play('ButtonDown');
 
-        this.PlayText = this.add.bitmapText( config.width / 2, config.height / 2 + 50, "pixelFont",  "Play", 10).setOrigin(0.5,0.5);
+        this.PlayText = this.add.bitmapText( config.width / 2, config.height / 3 * 2 + 50, "pixelFont",  "Play", 10).setOrigin(0.5,0.5);
+
+
+        
 
 
         var _this = this;
