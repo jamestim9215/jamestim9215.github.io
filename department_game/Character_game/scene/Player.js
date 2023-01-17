b6356160a55d762d6c54fd63ab8eb190e79f0a5b
Ray01 <ray01.lin@gigabyte.com>
2023-01-18 00:30:25 +0800

    UPDATE

--- department_game/Character_game/scene/Player.js
+++ department_game/Character_game/scene/Player.js
@@ -33,33 +33,13 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
 
         this.create();
         this.setAnims();
-        // this.setOrigin(0,0);
-
-        // this.setSize(16, 32);
-        // this.setOffset(24, 16);
-
 
         this.MoveX = 0;
         this.MoveY = 0;
 
-        // this.moveSpeed = 30;
-        // this.moveSpeed = 50;
         this.moveSpeed = 100;
         this.runSpeed = 2;
         this.isAttack = false;
-
-        // this.name = this.scene.add.bitmapText( x, y, "pixelFont",  _playerInfo.Name, 10).setOrigin(1,1);
-
-
-        // console.log("add player : " +_playerInfo.Name);
-
-        // this.name = this.scene.add.text(_x, _y, _playerInfo.Name, {
-        //     fontFamily: 'Arial',
-        //     color: type=='Player'?'#00ff00':'#ffffff'
-        // }).setFontSize(10).setOrigin(0.5);
-        // this.name.trackSprite(this, 0, 0, true);
-
-
     }
 
 
@@ -75,180 +55,7 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
         this.setTexture(keyName);
 
     }
-
     create() {
-        var _this = this;
-        var keyName = 'Character';
-        var keyNameSword = 'CharacterSword';
-
-        //站著
-        this.anims.create({
-            key: "CharacterIdleDown",
-            frames: this.anims.generateFrameNumbers(keyName, { frames: [0, 1, 2, 3, 4, 5, 6, 7] }),
-            frameRate: 10,
-            repeat: -1
-        })
-        this.anims.create({
-            key: "CharacterIdleRight",
-            frames: this.anims.generateFrameNumbers(keyName, { frames: [10, 11, 12, 13, 14, 15, 16, 17] }),
-            frameRate: 10,
-            repeat: -1
-        })
-        this.anims.create({
-            key: "CharacterIdleUp",
-            frames: this.anims.generateFrameNumbers(keyName, { frames: [20, 21, 22, 23, 24, 25, 26, 27] }),
-            frameRate: 10,
-            repeat: -1
-        })
-        this.anims.create({
-            key: "CharacterIdleLeft",
-            frames: this.anims.generateFrameNumbers(keyName, { frames: [30, 31, 32, 33, 34, 35, 36, 37] }),
-            frameRate: 10,
-            repeat: -1
-        })
-
-        //走路
-        this.anims.create({
-            key: "CharacterWalkDown",
-            frames: this.anims.generateFrameNumbers(keyName, { frames: [40, 41, 42, 43, 44, 45, 46, 47] }),
-            frameRate: 15,
-            repeat: -1
-        })
-        this.anims.create({
-            key: "CharacterWalkRight",
-            frames: this.anims.generateFrameNumbers(keyName, { frames: [50, 51, 52, 53, 54, 55, 56, 57] }),
-            frameRate: 15,
-            repeat: -1
-        })
-        this.anims.create({
-            key: "CharacterWalkUp",
-            frames: this.anims.generateFrameNumbers(keyName, { frames: [60, 61, 62, 63, 64, 65, 66, 67] }),
-            frameRate: 15,
-            repeat: -1
-        })
-        this.anims.create({
-            key: "CharacterWalkLeft",
-            frames: this.anims.generateFrameNumbers(keyName, { frames: [70, 71, 72, 73, 74, 75, 76, 77] }),
-            frameRate: 15,
-            repeat: -1
-        })
-
-
-        //跑步
-        this.anims.create({
-            key: "CharacterRunDown",
-            frames: this.anims.generateFrameNumbers(keyName, { frames: [80, 81, 82, 83, 84, 85, 86, 87] }),
-            frameRate: 20,
-            repeat: -1
-        })
-        this.anims.create({
-            key: "CharacterRunRight",
-            frames: this.anims.generateFrameNumbers(keyName, { frames: [90, 91, 92, 93, 94, 95, 96, 97] }),
-            frameRate: 20,
-            repeat: -1
-        })
-        this.anims.create({
-            key: "CharacterRunUp",
-            frames: this.anims.generateFrameNumbers(keyName, { frames: [100, 101, 102, 103, 104, 105, 106, 107] }),
-            frameRate: 20,
-            repeat: -1
-        })
-        this.anims.create({
-            key: "CharacterRunLeft",
-            frames: this.anims.generateFrameNumbers(keyName, { frames: [110, 111, 112, 113, 114, 115, 116, 117] }),
-            frameRate: 20,
-            repeat: -1
-        })
-
-        //攻擊 往右揮動
-        this.anims.create({
-            key: "CharacterAttackDown",
-            frames: this.anims.generateFrameNumbers(keyName, { frames: [120, 121, 122, 123, 124, 125] }),
-            frameRate: 20,
-            repeat: 0
-        })
-        this.anims.create({
-            key: "CharacterAttackRight",
-            frames: this.anims.generateFrameNumbers(keyName, { frames: [130, 131, 132, 133, 134, 135] }),
-            frameRate: 20,
-            repeat: 0
-        })
-        this.anims.create({
-            key: "CharacterAttackUp",
-            frames: this.anims.generateFrameNumbers(keyName, { frames: [140, 141, 142, 143, 144, 145] }),
-            frameRate: 20,
-            repeat: 0
-        })
-        this.anims.create({
-            key: "CharacterAttackLeft",
-            frames: this.anims.generateFrameNumbers(keyName, { frames: [150, 151, 152, 153, 154, 155] }),
-            frameRate: 20,
-            repeat: 0
-        })
-
-
-        
-
-
-
-
-        
-        //攻擊事件
-        this.scene.input.keyboard.on('keydown', function (event) {
-            if (event.key === 'z' && _this.isAttack==false) {
-                _this.isAttack = true;
-                _this.stand = true;
-                _this.status = 'Attack';
-                let sword = _this.scene.swordHitbox;
-                sword.setActive(true).setVisible(true);
-                sword.body.setVelocityX(0);
-                sword.body.setVelocityY(0);
-                sword.x = _this.x;
-                sword.y = _this.y;
-                sword.isAttack = true;
-                if (_this.isUp == 'Up') {
-                    _this.anims.play('CharacterAttackUp', true);
-                    sword.y = _this.y - 16;
-                    sword.setSize(48, 24);
-                    sword.setOrigin(0.5,0.5);
-                    sword.body.setSize(48, 24);
-                }
-                if (_this.isUp == 'Down') {
-                    _this.anims.play('CharacterAttackDown', true);
-                    sword.y = _this.y + 16;
-                    sword.setSize(48, 24);
-                    sword.setOrigin(0.5,0.5);
-                    sword.body.setSize(48, 24);
-                }
-                if (_this.isUp == 'Right') {
-                    _this.anims.play('CharacterAttackRight', true);
-                    sword.x = _this.x + 16;
-                    sword.setSize(24, 48);
-                    sword.setOrigin(0.5,0.5);
-                    sword.body.setSize(24, 48);
-                }
-                if (_this.isUp == 'Left') {
-                    _this.anims.play('CharacterAttackLeft', true);
-                    sword.x = _this.x - 16;
-                    sword.setSize(24, 48);
-                    sword.setOrigin(0.5,0.5);
-                    sword.body.setSize(24, 48);
-                }
-                setTimeout(()=>{
-                    sword.x = _this.x;
-                    sword.y = _this.y;
-                    sword.setSize(16, 16);
-                    sword.setOrigin(0.5,0.5);
-                    sword.body.setSize(16, 16);
-                    sword.body.setVelocityX(0);
-                    sword.body.setVelocityY(0);
-                    sword.attackNum = 0;
-                    sword.setActive(false).setVisible(false);
-                    sword.isAttack = false;
-                    _this.isAttack = false;
-                }, 400)
-            }
-        });
 
     }
     update(cursors, keys) {
@@ -366,6 +173,64 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
                 }
                 this.scene.playerContainer.body.setVelocityX(0);
                 this.scene.playerContainer.body.setVelocityY(0);
+            }
+
+            if (keys.Z.isDown && this.isAttack==false) {
+                this.isAttack = true;
+                this.stand = true;
+                this.status = 'Attack';
+                let sword = this.scene.swordHitbox;
+                sword.setActive(true).setVisible(true);
+                sword.body.setVelocityX(0);
+                sword.body.setVelocityY(0);
+                sword.x = this.x;
+                sword.y = this.y;
+                sword.isAttack = true;
+                if (this.isUp == 'Up') {
+                    this.anims.play('CharacterAttackUp', true);
+                    sword.anims.play('SwordAttackUp', true);
+                    sword.y = this.y - 16;
+                    sword.setSize(48, 24);
+                    sword.setOrigin(0.5,0.5);
+                    sword.body.setSize(48, 24);
+                }
+                if (this.isUp == 'Down') {
+                    this.anims.play('CharacterAttackDown', true);
+                    sword.anims.play('SwordAttackDown', true);
+                    sword.y = this.y + 16;
+                    sword.setSize(48, 24);
+                    sword.setOrigin(0.5,0.5);
+                    sword.body.setSize(48, 24);
+                }
+                if (this.isUp == 'Right') {
+                    this.anims.play('CharacterAttackRight', true);
+                    sword.anims.play('SwordAttackRight', true);
+                    sword.x = this.x + 16;
+                    sword.setSize(24, 48);
+                    sword.setOrigin(0.5,0.5);
+                    sword.body.setSize(24, 48);
+                }
+                if (this.isUp == 'Left') {
+                    this.anims.play('CharacterAttackLeft', true);
+                    sword.anims.play('SwordAttackLeft', true);
+                    sword.x = this.x - 16;
+                    sword.setSize(24, 48);
+                    sword.setOrigin(0.5,0.5);
+                    sword.body.setSize(24, 48);
+                }
+                setTimeout(()=>{
+                    sword.x = this.x;
+                    sword.y = this.y;
+                    sword.setSize(16, 16);
+                    sword.setOrigin(0.5,0.5);
+                    sword.body.setSize(16, 16);
+                    sword.body.setVelocityX(0);
+                    sword.body.setVelocityY(0);
+                    sword.attackNum = 0;
+                    sword.setActive(false).setVisible(false);
+                    sword.isAttack = false;
+                    this.isAttack = false;
+                }, 400)
             }
         }
         // this.anims.play('CharacterIdleDown', true);
--- department_game/Character_game/scene/Preload.js
+++ department_game/Character_game/scene/Preload.js
@@ -1,4 +1,4 @@
-import { config,PlayerInfo } from '../config'
+import { config, PlayerInfo } from '../config'
 // import { socket } from '../socket'
 
 
@@ -62,34 +62,21 @@ export default class Preload extends Phaser.Scene {
 
 
         //載入所有角色Skin
-        // for (var i = 1; i <= 32; i++) {
-        //     var num = 0;
-        //     if (i < 10) {
-        //         num = '0' + i
-        //     } else {
-        //         num = i
-        //     }
-
-        //     this.load.spritesheet('Character' + i, "./assets/images/Character/cat/pipo-nekonin0" + num + ".png", {
-        //         frameWidth: 32,
-        //         frameHeight: 32
-        //     });
-
-        // }
 
         this.load.spritesheet('Character', "./assets/images/Character/GumdropSource_03/SpriteSheets/Hero_P1.png", {
             frameWidth: 64,
             frameHeight: 64
         });
+
         this.load.spritesheet('Sword', "./assets/images/Character/GumdropSource_03/SpriteSheets/Sword_FG.png", {
             frameWidth: 64,
             frameHeight: 64
         });
+
         this.load.spritesheet('Smear', "./assets/images/Character/GumdropSource_03/SpriteSheets/Smear.png", {
             frameWidth: 64,
             frameHeight: 64
         });
-
 
         this.load.spritesheet('Slime', "./assets/images/Slime 32x32.png", {
             frameWidth: 32,
@@ -101,27 +88,347 @@ export default class Preload extends Phaser.Scene {
 
         this.load.tilemapTiledJSON('map', './assets/map/GameMap.json')
         this.load.image('tiles', './assets/images/Map/Serene_Village_32x32.png')
-        
-    
+
+
     }
 
     create() {
+        var keyName = 'Character';
+
+        //站著
+        this.anims.create({
+            key: "CharacterIdleDown",
+            frames: this.anims.generateFrameNumbers(keyName, { frames: [0, 1, 2, 3, 4, 5, 6, 7] }),
+            frameRate: 10,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "CharacterIdleRight",
+            frames: this.anims.generateFrameNumbers(keyName, { frames: [10, 11, 12, 13, 14, 15, 16, 17] }),
+            frameRate: 10,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "CharacterIdleUp",
+            frames: this.anims.generateFrameNumbers(keyName, { frames: [20, 21, 22, 23, 24, 25, 26, 27] }),
+            frameRate: 10,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "CharacterIdleLeft",
+            frames: this.anims.generateFrameNumbers(keyName, { frames: [30, 31, 32, 33, 34, 35, 36, 37] }),
+            frameRate: 10,
+            repeat: -1
+        })
+
+        //走路
+        this.anims.create({
+            key: "CharacterWalkDown",
+            frames: this.anims.generateFrameNumbers(keyName, { frames: [40, 41, 42, 43, 44, 45, 46, 47] }),
+            frameRate: 15,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "CharacterWalkRight",
+            frames: this.anims.generateFrameNumbers(keyName, { frames: [50, 51, 52, 53, 54, 55, 56, 57] }),
+            frameRate: 15,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "CharacterWalkUp",
+            frames: this.anims.generateFrameNumbers(keyName, { frames: [60, 61, 62, 63, 64, 65, 66, 67] }),
+            frameRate: 15,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "CharacterWalkLeft",
+            frames: this.anims.generateFrameNumbers(keyName, { frames: [70, 71, 72, 73, 74, 75, 76, 77] }),
+            frameRate: 15,
+            repeat: -1
+        })
+
+
+        //跑步
+        this.anims.create({
+            key: "CharacterRunDown",
+            frames: this.anims.generateFrameNumbers(keyName, { frames: [80, 81, 82, 83, 84, 85, 86, 87] }),
+            frameRate: 20,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "CharacterRunRight",
+            frames: this.anims.generateFrameNumbers(keyName, { frames: [90, 91, 92, 93, 94, 95, 96, 97] }),
+            frameRate: 20,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "CharacterRunUp",
+            frames: this.anims.generateFrameNumbers(keyName, { frames: [100, 101, 102, 103, 104, 105, 106, 107] }),
+            frameRate: 20,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "CharacterRunLeft",
+            frames: this.anims.generateFrameNumbers(keyName, { frames: [110, 111, 112, 113, 114, 115, 116, 117] }),
+            frameRate: 20,
+            repeat: -1
+        })
+
+        //攻擊 往右揮動
+        this.anims.create({
+            key: "CharacterAttackDown",
+            frames: this.anims.generateFrameNumbers(keyName, { frames: [120, 121, 122, 123, 124, 125] }),
+            frameRate: 20,
+            repeat: 0
+        })
+        this.anims.create({
+            key: "CharacterAttackRight",
+            frames: this.anims.generateFrameNumbers(keyName, { frames: [130, 131, 132, 133, 134, 135] }),
+            frameRate: 20,
+            repeat: 0
+        })
+        this.anims.create({
+            key: "CharacterAttackUp",
+            frames: this.anims.generateFrameNumbers(keyName, { frames: [140, 141, 142, 143, 144, 145] }),
+            frameRate: 20,
+            repeat: 0
+        })
+        this.anims.create({
+            key: "CharacterAttackLeft",
+            frames: this.anims.generateFrameNumbers(keyName, { frames: [150, 151, 152, 153, 154, 155] }),
+            frameRate: 20,
+            repeat: 0
+        })
+
+        var keyNameSword = 'Sword';
+
+        //站著
+        this.anims.create({
+            key: "SwordIdleDown",
+            frames: this.anims.generateFrameNumbers(keyNameSword, { frames: [0, 1, 2, 3, 4, 5, 6, 7] }),
+            frameRate: 10,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "SwordIdleRight",
+            frames: this.anims.generateFrameNumbers(keyNameSword, { frames: [10, 11, 12, 13, 14, 15, 16, 17] }),
+            frameRate: 10,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "SwordIdleUp",
+            frames: this.anims.generateFrameNumbers(keyNameSword, { frames: [20, 21, 22, 23, 24, 25, 26, 27] }),
+            frameRate: 10,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "SwordIdleLeft",
+            frames: this.anims.generateFrameNumbers(keyNameSword, { frames: [30, 31, 32, 33, 34, 35, 36, 37] }),
+            frameRate: 10,
+            repeat: -1
+        })
+
+        //走路
+        this.anims.create({
+            key: "SwordWalkDown",
+            frames: this.anims.generateFrameNumbers(keyNameSword, { frames: [40, 41, 42, 43, 44, 45, 46, 47] }),
+            frameRate: 15,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "SwordWalkRight",
+            frames: this.anims.generateFrameNumbers(keyNameSword, { frames: [50, 51, 52, 53, 54, 55, 56, 57] }),
+            frameRate: 15,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "SwordWalkUp",
+            frames: this.anims.generateFrameNumbers(keyNameSword, { frames: [60, 61, 62, 63, 64, 65, 66, 67] }),
+            frameRate: 15,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "SwordWalkLeft",
+            frames: this.anims.generateFrameNumbers(keyNameSword, { frames: [70, 71, 72, 73, 74, 75, 76, 77] }),
+            frameRate: 15,
+            repeat: -1
+        })
+
+
+        //跑步
+        this.anims.create({
+            key: "SwordRunDown",
+            frames: this.anims.generateFrameNumbers(keyNameSword, { frames: [80, 81, 82, 83, 84, 85, 86, 87] }),
+            frameRate: 20,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "SwordRunRight",
+            frames: this.anims.generateFrameNumbers(keyNameSword, { frames: [90, 91, 92, 93, 94, 95, 96, 97] }),
+            frameRate: 20,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "SwordRunUp",
+            frames: this.anims.generateFrameNumbers(keyNameSword, { frames: [100, 101, 102, 103, 104, 105, 106, 107] }),
+            frameRate: 20,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "SwordRunLeft",
+            frames: this.anims.generateFrameNumbers(keyNameSword, { frames: [110, 111, 112, 113, 114, 115, 116, 117] }),
+            frameRate: 20,
+            repeat: -1
+        })
+
+        //攻擊 往右揮動
+        this.anims.create({
+            key: "SwordAttackDown",
+            frames: this.anims.generateFrameNumbers(keyNameSword, { frames: [120, 121, 122, 123, 124, 125] }),
+            frameRate: 20,
+            repeat: 0
+        })
+        this.anims.create({
+            key: "SwordAttackRight",
+            frames: this.anims.generateFrameNumbers(keyNameSword, { frames: [130, 131, 132, 133, 134, 135] }),
+            frameRate: 20,
+            repeat: 0
+        })
+        this.anims.create({
+            key: "SwordAttackUp",
+            frames: this.anims.generateFrameNumbers(keyNameSword, { frames: [140, 141, 142, 143, 144, 145] }),
+            frameRate: 20,
+            repeat: 0
+        })
+        this.anims.create({
+            key: "SwordAttackLeft",
+            frames: this.anims.generateFrameNumbers(keyNameSword, { frames: [150, 151, 152, 153, 154, 155] }),
+            frameRate: 20,
+            repeat: 0
+        })
+
+        var keyNameSmear = 'Smear';
+
+        //站著
+        this.anims.create({
+            key: "SmearIdleDown",
+            frames: this.anims.generateFrameNumbers(keyNameSmear, { frames: [0, 1, 2, 3, 4, 5, 6, 7] }),
+            frameRate: 10,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "SmearIdleRight",
+            frames: this.anims.generateFrameNumbers(keyNameSmear, { frames: [10, 11, 12, 13, 14, 15, 16, 17] }),
+            frameRate: 10,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "SmearIdleUp",
+            frames: this.anims.generateFrameNumbers(keyNameSmear, { frames: [20, 21, 22, 23, 24, 25, 26, 27] }),
+            frameRate: 10,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "SmearIdleLeft",
+            frames: this.anims.generateFrameNumbers(keyNameSmear, { frames: [30, 31, 32, 33, 34, 35, 36, 37] }),
+            frameRate: 10,
+            repeat: -1
+        })
+
+        //走路
+        this.anims.create({
+            key: "SmearWalkDown",
+            frames: this.anims.generateFrameNumbers(keyNameSmear, { frames: [40, 41, 42, 43, 44, 45, 46, 47] }),
+            frameRate: 15,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "SmearWalkRight",
+            frames: this.anims.generateFrameNumbers(keyNameSmear, { frames: [50, 51, 52, 53, 54, 55, 56, 57] }),
+            frameRate: 15,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "SmearWalkUp",
+            frames: this.anims.generateFrameNumbers(keyNameSmear, { frames: [60, 61, 62, 63, 64, 65, 66, 67] }),
+            frameRate: 15,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "SmearWalkLeft",
+            frames: this.anims.generateFrameNumbers(keyNameSmear, { frames: [70, 71, 72, 73, 74, 75, 76, 77] }),
+            frameRate: 15,
+            repeat: -1
+        })
+
+
+        //跑步
+        this.anims.create({
+            key: "SmearRunDown",
+            frames: this.anims.generateFrameNumbers(keyNameSmear, { frames: [80, 81, 82, 83, 84, 85, 86, 87] }),
+            frameRate: 20,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "SmearRunRight",
+            frames: this.anims.generateFrameNumbers(keyNameSmear, { frames: [90, 91, 92, 93, 94, 95, 96, 97] }),
+            frameRate: 20,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "SmearRunUp",
+            frames: this.anims.generateFrameNumbers(keyNameSmear, { frames: [100, 101, 102, 103, 104, 105, 106, 107] }),
+            frameRate: 20,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "SmearRunLeft",
+            frames: this.anims.generateFrameNumbers(keyNameSmear, { frames: [110, 111, 112, 113, 114, 115, 116, 117] }),
+            frameRate: 20,
+            repeat: -1
+        })
+
+        //攻擊 往右揮動
+        this.anims.create({
+            key: "SmearAttackDown",
+            frames: this.anims.generateFrameNumbers(keyNameSmear, { frames: [120, 121, 122, 123, 124, 125] }),
+            frameRate: 20,
+            repeat: 0
+        })
+        this.anims.create({
+            key: "SmearAttackRight",
+            frames: this.anims.generateFrameNumbers(keyNameSmear, { frames: [130, 131, 132, 133, 134, 135] }),
+            frameRate: 20,
+            repeat: 0
+        })
+        this.anims.create({
+            key: "SmearAttackUp",
+            frames: this.anims.generateFrameNumbers(keyNameSmear, { frames: [140, 141, 142, 143, 144, 145] }),
+            frameRate: 20,
+            repeat: 0
+        })
+        this.anims.create({
+            key: "SmearAttackLeft",
+            frames: this.anims.generateFrameNumbers(keyNameSmear, { frames: [150, 151, 152, 153, 154, 155] }),
+            frameRate: 20,
+            repeat: 0
+        })
 
 
         this.anims.create({
             key: "SlimeIdle",
-            frames: this.anims.generateFrameNumbers("Slime", { frames: [0,1,2,3] }),
+            frames: this.anims.generateFrameNumbers("Slime", { frames: [0, 1, 2, 3] }),
             frameRate: 5,
             repeat: -1
         })
-        
+
         this.anims.create({
             key: "SlimeJump",
-            frames: this.anims.generateFrameNumbers("Slime", { frames: [0,1,2,3] }),
+            frames: this.anims.generateFrameNumbers("Slime", { frames: [0, 1, 2, 3] }),
             frameRate: 5,
             repeat: -1
         })
-        
+
         this.scene.start("GamePlay");
         // this.scene.start("GameHouse");
         // this.scene.start("GameHome");
