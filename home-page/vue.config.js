0af15c40c0021a61f5ec3800c9b41d8db145ce4d
Ray <ray.lin@gigabyte.com>
2023-04-24 18:14:26 +0800

    更新

--- home-page/vue.config.js
+++ home-page/vue.config.js
@@ -2,10 +2,8 @@
 
 module.exports = {
     publicPath: process.env.NODE_ENV === "production" 
-    ? 
-    "./" 
-    : "/",
-    
+    ? "./" 
+    : "./",
   
     // 在yarn build時 生成檔案的目錄 default:'dist'
     outputDir: "./dist",
@@ -13,6 +11,5 @@ module.exports = {
   
     // 放置生成的靜態資源 (js、css、img、fonts) 的目錄。default: ''
     assetsDir: "assets",
-
-  };
-  
+    
+};
