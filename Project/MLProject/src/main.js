5b0b6fa096a46c7e23bd9021a99659ed94e1c42c
jamestim9215 <jamestim9215@gmail.com>
2023-08-05 22:28:41 +0800

    remove ML Porject

--- Project/MLProject/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import 'material-icons/iconfont/material-icons.css';
-import './style.css'
-
-let app = createApp(App);
-app.use(router);
-app.mount('#app');
