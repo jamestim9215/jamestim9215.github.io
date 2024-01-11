49c1ed25bbc8ca49b093f1a7a5393825edb452dc
Ray01 <ray01.lin@gigabyte.com>
2024-01-11 16:24:02 +0800

    更新

--- speedometer_project/src/App.vue
+++ speedometer_project/src/App.vue
@@ -13,76 +13,17 @@ const options =  ref({
   maximumAge: 0
 });
 
-const previousPosition = ref(null);
-const previousTimestamp = ref(null);
-const speeds = ref([]);
 const filteredSpeed = ref(0);
+
+const speed = ref(0);
 
 
 const calculateSpeed = (position) => {
-  if (previousPosition.value !== null && previousTimestamp.value !== null) {
-    const distance = calculateDistance(
-      previousPosition.value.coords.latitude,
-      previousPosition.value.coords.longitude,
-      position.coords.latitude,
-      position.coords.longitude
-    );
-
-    const timeDifference = (position.timestamp - previousTimestamp.value) / 1000;
-    const newSpeed = Math.round((distance / timeDifference) * 3600);
-
-    // 濾除極端值
-    if (newSpeed > 1 && newSpeed < 150) {
-      // 將新速度添加到速度列表
-      speeds.value.push(newSpeed);
-
-      // 保留最後 N 個速度，這裡設定為10
-      const maxSpeeds = 10;
-      if (speeds.value.length > maxSpeeds) {
-        speeds.value.shift();
-      }
-
-      // 平滑速度
-      const smoothedSpeed = exponentialMovingAverage(newSpeed);
-
-      // 計算平均速度
-      const totalSpeed = speeds.value.reduce((acc, speed) => acc + speed, 0);
-      filteredSpeed.value = (speeds.value.length > 0) ? Math.round(totalSpeed / speeds.value.length) : 0;
-    }
-  }
-
-  // 更新前一個位置和時間
-  previousPosition.value = position;
-  previousTimestamp.value = position.timestamp;
+  const currentSpeed = position.coords.speed || 0;
+  console.log(position.coords.speed);
+  speed.value = (currentSpeed * 3600) / 1000;
 };
 
-// 指數移動平均
-const exponentialMovingAverage = (newValue, alpha = 0.2) => {
-  if (typeof exponentialMovingAverage.prevValue === 'undefined') {
-    exponentialMovingAverage.prevValue = newValue;
-  }
-
-  const smoothedValue = alpha * newValue + (1 - alpha) * exponentialMovingAverage.prevValue;
-  exponentialMovingAverage.prevValue = smoothedValue;
-
-  return Math.round(smoothedValue);
-};
-
-const calculateDistance = (lat1, lon1, lat2, lon2) => {
-  const R = 6371;
-  const dLat = deg2rad(lat2 - lat1);
-  const dLon = deg2rad(lon2 - lon1);
-  const a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const distance = R * c;
-  return distance;
-};
-
-const deg2rad = (deg) => {
-  return deg * (Math.PI / 180);
-};
 
 
 const randomSpeed = () => {
@@ -108,13 +49,13 @@ onMounted(() => {
 <template>
   <div class="digital-box">
     <div class="randomSpeed">
-      <p>時速: {{ filteredSpeed }} km/h</p>
+      <p>時速: {{ speed }} km/h</p>
       <button @click="randomSpeed()"> random speed </button>
       <button @click="chnageSpeedometerType()"> change type </button>
     </div>
-    <Speedometer01 :speed="filteredSpeed" v-if="SpeedometerType==1"/>
-    <Speedometer02 :speed="filteredSpeed" v-if="SpeedometerType==2"/>
-    <Speedometer03 :speed="filteredSpeed" v-if="SpeedometerType==3"/>
+    <Speedometer01 :speed="speed" v-if="SpeedometerType==1"/>
+    <Speedometer02 :speed="speed" v-if="SpeedometerType==2"/>
+    <Speedometer03 :speed="speed" v-if="SpeedometerType==3"/>
   </div>
 </template>
 
